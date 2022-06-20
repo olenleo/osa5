3e5889e24e50938b8aef4d1a3d714866f9f1c9a9
Leo Niemi <niemi.leo@gmail.com>
2022-06-20 11:18:04 +0300

    Notifications and submitBlog functionality

---
+++ bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const Notification = ({message, notificationType}) => {
+    if (message === null) {   
+      return null
+    }
+    return (
+        <div className={notificationType} > 
+        <p>{message}</p>
+        </div>)
+  }
+
+
+export default Notification
